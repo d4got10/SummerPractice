01c57859a629576184a68eee6a8b662b26e64ea8
Alexey Burakov <burakov.aa@students.dvfu.ru>
2020-07-22 21:09:10 +1000

    Added output info

--- GraphWindowUnit.pas
+++ GraphWindowUnit.pas
@@ -23,15 +23,8 @@ begin
   end;
 end;
 
-procedure CreateWindow;
-var
-  w: real;
+procedure DrawGrid;
 begin
-  w := 0.92;
-  Window.SetSize(700, 700);
-  Window.IsFixedSize := true;
-  Window.CenterOnScreen;
-  Window.Caption := 'Визуализация алгоритмов поиска кратчайшего пути';
   SetMathematicCoords(0, 20, 0, true);
   for var x:=0 to n-1 do
     for var y:=0 to n-1 do begin
@@ -44,19 +37,29 @@ begin
         5: Brush.Color := Colors.Green;   // начало
         6: Brush.Color := Colors.Red;     // конец
       end;
-      Rectangle(x + 0.04, y + 0.04, w, w); (* рисуем клетку *)
+      Rectangle(x + 0.04, y + 0.04, 0.92, 0.92); (* рисуем клетку *)
     end;
+end;
+
+procedure CreateWindow;
+begin
+  Window.SetSize(700, 700);
+  Window.CenterOnScreen;
+  Window.IsFixedSize := true;
+  Window.Caption := 'Визуализация алгоритмов поиска кратчайшего пути';
+  SetMathematicCoords(0, 20, 0, true);
+  DrawGrid;
 end;
 
 procedure Animation;
 begin
-  BeginFrameBasedAnimation(CreateWindow, 30);
+  BeginFrameBasedAnimation(DrawGrid, 30);
 end;
 
 begin
   machine := new StateMachine(20);
   machine.OnGridChange := Animation;
-  Pen.Color := RGB(255,255,255);
+  Pen.Color := Colors.White;
   CreateWindow;
   OnMouseDown := ColorCell;
   OnMouseMove := ColorCell;
