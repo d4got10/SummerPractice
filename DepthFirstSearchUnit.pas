d19995e4c469aad0e4f073d92b92e936b4bd8fb6
d4got10 <izotov.da@students.dvfu.ru>
2020-07-22 19:36:23 +1000

    Added DFS and Diekstra algorithms. Not working

---
+++ DepthFirstSearchUnit.pas
@@ -0,0 +1,85 @@
+﻿Unit DepthFirstSearchUnit;
+
+  Interface
+    uses AlgorithmUnit;
+    
+    type
+      Stack = ^Element;
+      Element = record 
+        data : Point;
+        next : Stack;
+      end; 
+      DepthFirstSearch = class(Algorithm)
+        private
+          _point, _section : Point;
+          _dist : Grid;
+          _prev : intArray;
+          _head, _node : Stack
+          _timeMark : intArray;
+          _constantMark : intArray;
+          //Добавить, если нужно 
+        public
+          constructor Create(gridSize : integer; start, finish : Point);
+          begin
+            Inherited Create(gridSize, start, finish);
+            _grid := new intArray[_gridSize];
+            for var i := 0 to _gridSize - 1 do
+              _grid[i] := new integer[_gridSize];
+            _point := start;
+            _section := start;
+            _gridMark[_point.x][_point.y] := TRUE;
+            _head := nil;
+          end;
+          procedure Step(); override;
+          function GetGridLayout() : Grid; override;
+       end;
+       
+Implementation
+  function DepthFirstSearch.Neighbour(var section : Point; cell : Point) : boolean;
+    begin
+      for var x := -1 to 1 do
+        for var y := -1 to 1 do
+          if not _gridMark[cell.x + x][cell.y + y] and IsWalkable(cell.x + x, cell.y + y)
+            then begin
+              Neighbour := true;
+              section.x := cell.x + x;
+              section.y := cell.y + y;
+             break;
+            end
+          else Neighbour := false;       
+    end;
+  procedure DepthFirstSearch.Step();
+    begin
+      if not _gridMark[finish.x][finish.y] then
+        begin
+          if Neighbour(_section, _point) then
+            begin
+              _node := _head;
+              new(_head);
+              _head^.data := _point;
+              _head^.next := _node;
+              _point := _section;
+              _gridMark [_point.x][_point.y] := true;
+            end;
+          else if _head <> nil then
+            begin
+              _node := _head^.next;
+              _point := _head^.data;
+              dispose(_head);
+              _head^.next := _node;
+            end
+          //else (если не найден выход)
+        end
+      else if _onFinish <> nil then _onFinish;
+        if _onStep <> nil then _onStep;  
+     end;
+  
+  function DepthFirstSearch.GetGridLayout() : Grid;
+    begin
+      var temp := new intArray[_gridSize];
+      for var i := 0 to _gridSize-1 do
+        temp[i] := new integer[_gridSize];
+      
+      GetGridLayout := temp
+    end;
+end.      
---
+++ DiekstraUnit.pas
@@ -0,0 +1,71 @@
+﻿Unit DiekstraUnit;
+
+Interface 
+  uses AlgorithmUnit;
+  
+  type 
+    Diekstra = class(Algorithm);
+      private
+       _dist : Grid;
+       _prev : intArray;
+       _point : Point;
+       _length : integer
+      //Заполнить, если нужно будет
+      public
+        constructor Create(gridSize : integer; start, finish : Point);
+        begin
+          Inherited Create(gridSize, start, finish);
+          _grid := new intArray[_gridSize];
+          for var i := 0 to _gridSize - 1 do
+            _grid[i] := new integer[_gridSize];
+          _point := start;
+          _length := MaxInt;
+        end;
+        procedure Step(); override;
+        function GetGridLayout() : Grid; override;
+    end;
+    
+Implementation
+  function Diekstra.Weight(from, there: Point) : Integer;
+  begin
+    if IsWalkable(from.x, from.y) and IsWalkable(there.x, there.y) 
+    and Distance(from.x, from.y, there.x, there.y) < 20  then
+      Weight := Distance(from.x, from.y, there.x, there.y);
+    else 
+      Weight := MaxInt;    
+  end;               
+  procedure Diekstra.Step();
+  begin
+    if not _gridMark[finish.x][finish.y] then 
+      begin
+        for var x := 0 to _gridSize - 1 do
+          for var y := 0 to _gridSize - 1 do
+            if not _gridMark[x][y] and (_dist[x][y] > _dist[_point.x][_point.y] 
+            + Weight[_point.x, point.y]) then 
+              begin
+                _dist[x][y] := _dist[_point.x][_point.y] + Weight[_point.x][point.y];
+                _prev[x][y] := _point;
+              end;
+        for var x := 0 to _gridSize - 1 do
+          for var y := 0 to _gidSize - 1 do
+            if not _gridMark[x][y] then
+              if _weight > _dist[x][y] then
+                begin
+                  _weight := _dist[x][y];
+                  _point := _dist[x][y];
+                end;
+        _gridMark[_point.x][_point.y] := TRUE;
+      end
+     else if _onFinish <> nil then _onFinish;
+     if _onStep <> nil then _onStep;
+  end; 
+  
+  function Diekstra.GetGridLayout() : Grid;
+    begin
+      var temp := new intArray[_gridSize];
+      for var i := 0 to _gridSize-1 do
+        temp[i] := new integer[_gridSize];
+      
+      GetGridLayout := temp
+    end;
+end.      
